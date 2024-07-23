43356885f16ca3f3d92789f3f3342bfb45da9b45
Surya-Sandeep <89667149+Surya-Sandeep@users.noreply.github.com>
2024-07-23 18:46:15 +0100

    Update PDF-bot.py

--- pages/PDF-bot.py
+++ pages/PDF-bot.py
@@ -1,9 +1,11 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.vectorstores.faiss import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
