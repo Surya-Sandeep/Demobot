448796ae0b0d4c4fe529284f8b6fb32508d205ec
Surya-Sandeep <89667149+Surya-Sandeep@users.noreply.github.com>
2024-10-26 12:59:11 +0100

    Update Paraphrase.py

--- pages/Paraphrase.py
+++ pages/Paraphrase.py
@@ -1,21 +1,20 @@
 import streamlit as st
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 import torch
 
-# Load the T5 model and tokenizer
-model_name = "t5-base"
-tokenizer = T5Tokenizer.from_pretrained(model_name)
-model = T5ForConditionalGeneration.from_pretrained(model_name)
+# Load the Pegasus model and tokenizer
+model_name = "google/pegasus-large"
+tokenizer = PegasusTokenizer.from_pretrained(model_name)
+model = PegasusForConditionalGeneration.from_pretrained(model_name)
 
 # Function to generate paraphrases
 def generate_paraphrases(input_text):
     # Prepare the input for the model
-    input_text = "paraphrase: " + input_text
     input_ids = tokenizer.encode(input_text, return_tensors="pt", max_length=512, truncation=True)
 
     # Generate paraphrases
     with torch.no_grad():
-        output_ids = model.generate(input_ids, max_length=512, num_beams=5, num_return_sequences=5, early_stopping=True)
+        output_ids = model.generate(input_ids, num_beams=5, num_return_sequences=5, max_length=60, early_stopping=True)
 
     # Decode the generated paraphrases
     paraphrases = [tokenizer.decode(output_id, skip_special_tokens=True) for output_id in output_ids]
